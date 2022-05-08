7c5afb5da09c28cc4cc7a59e44536a68cbec5f50
Castro Maciel <castromaciel00@gmail.com>
2022-05-08 20:42:36 -0300

    Component Compound v2

---
+++ src/CompundPattern/components/ProductCard.tsx
@@ -0,0 +1,85 @@
+import styles from '../styles/styles.module.css'
+import noimg from '../assets/no-image.jpg'
+import { useProduct } from '../hooks/useProduct';
+import { ReactElement, useContext } from 'react';
+import { createContext } from 'vm';
+
+interface Props{
+  children?: ReactElement | ReactElement[ ]
+  product: Product;
+}
+
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+interface ProdcutContextProps {
+  counter: number;
+  increaseBy: (value: number) => void;
+  product: Product;
+}
+
+
+const ProductContext = createContext( { } as ProdcutContextProps  );
+const { Provider } = ProductContext;
+
+export const ProductImage = ({title = '', img= ''}) => {
+
+  const { product } = useContext(ProductContext);
+  let imgToShow: string;
+
+  if( img ) imgToShow = img;
+  else if ( product.img ) imgToShow = product.img;
+  else imgToShow = noimg;
+
+  return (
+    <img className={ styles.productImg } src={imgToShow } alt={ title } />
+  )
+}
+
+export const ProductTitle = ({title}: {title: string} ) => {
+
+  return (
+    <span className={ styles.productDescription }>{title}</span>
+  )
+}
+
+export const ProductButtons = ( ) => {
+
+  const {increaseBy, counter} = useContext(ProductContext);
+
+  return(
+    <div className={styles.buttonsContainer}>
+      <button type="button" className={styles.buttonMinus} onClick={() => increaseBy(-1)}> - </button>
+      <div className={styles.countLabel}> {counter} </div>
+      <button type="button" className={styles.buttonAdd} onClick={() => increaseBy(1)}> + </button>
+    </div>
+
+  )
+}
+
+
+export const ProductCard = ({ children, product }: Props) => {
+
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <Provider value={{
+      counter,
+      increaseBy,
+      product
+    }}>
+      <div className={ styles.productCard }>
+
+        { children }
+
+      </div>
+    </Provider>
+  )
+};
+
+ProductCard.Image = ProductImage;
+ProductCard.Title = ProductTitle;
+ProductCard.Buttons = ProductButtons;
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import ShoppingPage from '../CompundPattern/Pages/ShoppingPage';
 
 import logo from '../logo.svg'
 
@@ -15,7 +16,7 @@ function Navigation() {
             <NavLink to='/'
               className={ ({isActive}) => isActive ? 'nav-active' : ''}
             >
-              Home 
+              Shopping 
             </NavLink>
           </li>
           <li>
@@ -38,7 +39,7 @@ function Navigation() {
       <Routes>
         <Route path="about" element={<h1>About</h1>} />
         <Route path="users" element={<h1>Users</h1>} />
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<ShoppingPage />} />
 
         <Route path="/*" element={ <Navigate to='home' replace /> }/>
       </Routes>
